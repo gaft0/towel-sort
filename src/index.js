ddf86bb56a88a07f8ec8ffc1c014a6687816e4fe
Александр Плесовских <sasha301106@gmail.com>
2025-09-04 22:44:15 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const array = [];
+  if (!Array.isArray(matrix)) {
+    return [];
+  }
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    const currentRow = matrix[i];
+    if (i % 2 === 0) {
+      for (let j = 0; j < currentRow.length; j += 1) {
+        array.push(currentRow[j]);
+      }
+    } else {
+      for (let j = currentRow.length - 1; j >= 0; j -= 1) {
+        array.push(currentRow[j]);
+      }
+    }
+  }
+  return array;
 };
